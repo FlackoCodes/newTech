16f4585416b537bac7f31fa50df0d591d07c3b0f
Flacko <lesliejoe2k2@gmail.com>
2025-03-10 23:45:07 +0000

    added links to utility pages

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,4 +1,5 @@
-import { footerData } from "../data";
+import { Link } from "react-router-dom";
+import { footerData, utilityPages } from "../data";
 import logo from "../images/cta/Logo (1).png"
 import { FcCheckmark } from "react-icons/fc";
 
@@ -17,17 +18,29 @@ export default function Footer() {
                 </div>
             </div>
             <div className="grid grid-cols-1 md:grid-cols-2 gap-8 py-6">
-                <div className="border-r border-[#E0E0E0] flex flex-col md:flex-row justify-between items-center">
-
+                <div className="border-r border-[#E0E0E0]">
+                    <div></div>
+                    <div className="tracking-tight">
+                        <header>
+                            <h4 className="font-[Mullish] text-[#2F2F2F] text-2xl font-bold">Utility Pages</h4>
+                        </header>
+                        {
+                            utilityPages.map((item) => (
+                                <div key={item.id} className="flex gap-2 items-center my-2">
+                                    <Link to={`/${item.page}`} className="font-semibold font-mono  text-start text-[#808080]">{item.page}</Link>
+                                </div>
+                            ))
+                        }
+                    </div>
                 </div>
                 <div className="bg-[#2F2F2F] rounded-2xl grid grid-cols-2 gap-2 p-6">
                     <div className="border-r border-[#E0E0E0] grid place-content-center align-middle">
-                        <button className="bg-gradient-to-r from-[#5956E9] to-[#E541ED] py-2 px-3 rounded-full text-white cursor-pointer">Click</button>
+                        <button className="bg-gradient-to-r from-[#5956E9] to-[#E541ED] py-2 px-3 rounded-full text-white cursor-pointer font-bold">Click</button>
                         <div className="flex items-center gap-2 my-4 font-[Mulish] text-white">
                             <h2 className="align-middle text-2xl font-bold capitalize">startup</h2>
                             <span className="font-extrabold text-4xl">$25</span>
                         </div>
-                        <button className="bg-[#424242] text-white px-3 py-2 rounded-full cursor-pointer">Popular</button>
+                        <button className="bg-[#424242] text-white px-3 py-2 rounded-full cursor-pointer font-bold">Popular</button>
                     </div>
                     <div className="py-6 px-2">
                         {footerData.map((item) => (
--- src/data.ts
+++ src/data.ts
@@ -26,9 +26,64 @@ export const footerData = [
   },
 ];
 
+export const resourcesPages = [
+  {
+    id: 1,
+    page: "Home",
+  },
+  {
+    id: 2,
+    page: "About",
+  },
+  {
+    id: 3,
+    page: "Contact",
+  },
+  {
+    id: 4,
+    page: "Pricing",
+  },
+  {
+    id: 5,
+    page: "Blog",
+  },
+  {
+    id: 6,
+    page: "Blog Post ",
+  },
+  {
+    id: 7,
+    page: "Pricing",
+  },
+  {
+    id: 8,
+    page: "Pricing Single",
+  },
+];
 
 export const utilityPages = [
-    {
-        id: 1,
-        page: "Contact",}
-]
+  {
+    id: 1,
+    page: "Start here",
+  },
+  {
+    id: 2,
+    page: "Style guide",
+  },
+  {
+    id: 3,
+    page: "Password protected",
+  },
+  {
+    id: 4,
+    page: "404 Not found",
+  },
+  {
+    id: 5,
+    page: "Licenses",
+  },
+  {
+    id: 6,
+    page: "Changelog",
+  },
+];
