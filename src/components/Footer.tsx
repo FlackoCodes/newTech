e423a67cec442dedaea574780d4fa087a43505ef
Flacko <lesliejoe2k2@gmail.com>
2025-03-12 19:32:31 +0000

    fixed footer responsivness

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -18,7 +18,7 @@ export default function Footer() {
                 </div>
             </div>
             <div className="grid grid-cols-1 md:grid-cols-2 gap-10 py-6">
-                <div className="border-r border-[#E0E0E0] grid grid-cols-1 md:grid-cols-2 gap-24">
+                <div className="md:border-r border-[#E0E0E0] grid grid-cols-1 md:grid-cols-2 gap-24">
                     <div className="tracking-tight">
                         <header>
                             <h4 className="font-sans text-[#2F2F2F] text-2xl font-bold">Resources</h4>
@@ -57,8 +57,8 @@ export default function Footer() {
                         }
                     </div>
                 </div>
-                <div className="bg-[#2F2F2F] rounded-2xl grid grid-cols-2 gap-2 p-6">
-                    <div className="border-r border-[#E0E0E0] grid place-content-center align-middle">
+                <div className="bg-[#2F2F2F] rounded-2xl grid grid-cols-1 md:grid-cols-2 gap-2 p-6">
+                    <div className="md:border-r border-[#E0E0E0] grid place-content-center align-middle">
                         <button className="bg-gradient-to-r from-[#5956E9] to-[#E541ED] py-2 px-3 rounded-full text-white cursor-pointer font-bold">Click</button>
                         <div className="flex items-center gap-2 my-4 font-[Mulish] text-white">
                             <h2 className="align-middle text-2xl font-bold capitalize">startup</h2>
