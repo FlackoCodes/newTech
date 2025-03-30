63d179cd3aa56143ff0f1a87d769d3f97ce052da
Flacko <lesliejoe2k2@gmail.com>
2025-03-30 21:04:08 +0000

    fixes

--- src/pages/HomePage/HomeFeatures.tsx
+++ src/pages/HomePage/HomeFeatures.tsx
@@ -1,6 +1,6 @@
-import { FcCheckmark } from "react-icons/fc"
-import featImage from "../../images/img/featImage.png"
-import { features } from "../../data"
+import { FcCheckmark } from "react-icons/fc";
+import featImage from "../../images/img/featImage.png";
+import { features } from "../../data";
 
 export default function HomeFeatures() {
     return (
@@ -8,9 +8,13 @@ export default function HomeFeatures() {
             <div className="grid grid-cols-1 md:grid-cols-2">
                 <div className="flex flex-col gap-2 text-start">
                     <header>
-                        <h4 className="text-start w-[200px] my-0 font-[Poppins] text-2xl font-bold">The Tool That Feels Like a..</h4>
+                        <h4 className="text-start w-[200px] my-0 font-[Poppins] text-2xl font-bold">
+                            The Tool That Feels Like a..
+                        </h4>
                     </header>
-                    <p className="text-[#808080] text-start md:w-[500px]">Building an enterprise level site doesn't need nightmare or cost your thousands. Felix is purpose built for ease of use and complete flexibility to create even the most powerful of products.</p>
+                    <p className="text-[#808080] text-start md:w-[500px]">
+                        Building an enterprise-level site doesn't need nightmare or cost your thousands. Felix is purpose-built for ease of use and complete flexibility to create even the most powerful of products.
+                    </p>
                     <div>
                         <ul className="flex flex-col gap-2 text-black font-[Poppins] text-sm font-normal mt-4">
                             <div className="flex gap-1 items-center">
@@ -27,26 +31,34 @@ export default function HomeFeatures() {
                             </div>
                         </ul>
                     </div>
+                    <button className="my-2 rounded-full bg-black text-white md:py-2 py-2 px-4 md:px-6 font-[Poppins] cursor-grab w-fit">Get Started</button>
                 </div>
                 <img src={featImage} alt="schedule image" />
             </div>
+
             <div className="my-8">
                 <header className="text-center font-bold text-2xl text-[#2F2F2F] my-2">
                     <h4>Simple, yet Powerful Features</h4>
                 </header>
-                {
-                    features.map((feature) => (
-                        <div className="grid grid-cols-1  lg:grid-cols-3 my-8">
-                            <div className="rounded border border-[#DCDCDC] p-8 ">
-                                <div key={feature.id} className="flex flex-col gap-2">
+
+                {/* Grid container for features */}
+                <div className="grid grid-cols-1 lg:grid-cols-3 gap-8">
+                    {
+                        features.map((feature) => (
+                            <div key={feature.id} className="rounded border border-[#DCDCDC] p-8">
+                                <div className="flex flex-col gap-2">
                                     <header>
-                                        <h5 className="py-2 px-4 text-[#2F2F2F] font-[Poppins] text-lg rounded-4xl bg-[#F6F6F6] inline ">
+                                        <h5 className="py-2 px-4 text-[#2F2F2F] font-[Poppins] text-lg rounded-4xl bg-[#F6F6F6] inline">
                                             {feature.header}
                                         </h5>
                                     </header>
                                     <div className="flex flex-col gap-2 border-b border-[#DCDCDC] my-2">
-                                        <h4 className="text-[#2F2F2F] font-[Poppins] text-[12px] md:text-[16px] font-normal md:font-bold">{feature.title}</h4>
-                                        <p className="text-[#808080] font-[Montserrat] text-sm font-normal mb-4">{feature.description}</p>
+                                        <h4 className="text-[#2F2F2F] font-[Poppins] text-[12px] md:text-[16px] font-normal md:font-bold">
+                                            {feature.title}
+                                        </h4>
+                                        <p className="text-[#808080] font-[Montserrat] text-sm font-normal mb-4">
+                                            {feature.description}
+                                        </p>
                                     </div>
                                     <div className="flex justify-between items-center my-1">
                                         <img src={feature.icon} alt="feature icon" className="w-6" />
@@ -54,9 +66,10 @@ export default function HomeFeatures() {
                                     </div>
                                 </div>
                             </div>
-                        </div>
-                    ))}
+                        ))
+                    }
+                </div>
             </div>
         </div>
-    )
+    );
 }
