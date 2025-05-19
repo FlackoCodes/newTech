864548593a35270b9ae5764cc9faa60d6388e23b
Flacko <lesliejoe2k2@gmail.com>
2025-05-19 21:36:14 +0000

    worked on hero section

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,5 +1,9 @@
+import heroBg from "../images/img/hero-bg.jpg"
+
 export const Hero = () => {
   return (
-    <div>Hero</div>
+    <div className="w-[90%] my-0 mx-auto mt-2 md:mt-10 lg:my-40 relative">
+      <div style={{ backgroundImage: `url(${heroBg})` }} className="bg-cover bg-center bg-no-repeat">hehr</div>
+    </div>
   )
 }
--- src/pages/HomePage/Hero.tsx
+++ src/pages/HomePage/Hero.tsx
@@ -1,5 +1,16 @@
+import heroBg from "../../images/img/hero-bg.jpg"
+
 export default function Hero() {
     return (
-        <div>Hero</div>
+        <div className="w-[90%] my-0 mx-auto relative">
+            <div style={{ backgroundImage: `url(${heroBg})` }} className="bg-cover bg-center bg-no-repeat rounded mt-4 mb-8">
+                <div className="flex items-center flex-col p-10">
+                    <h1 className="py-2 text-white font-bold md:text-2xl lg:text-3xl md:w-[600px] text-center">All In One Social Media Management Solution For Businesses</h1>
+                    <p className="text-white text-lg font-normal my-2 font-[poppins]">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+                    <button type="button" className="bg-white rounded-full my-2 font-normal py-2 px-6 md:text-lg">Create Free Account</button>
+                </div>
+            </div>
+        </div>
     )
 }
+
