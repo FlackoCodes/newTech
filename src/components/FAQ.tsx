e3e343abd2a24746277bd68fa979a979bc7bc457
Joseph Kofi Leslie Lartey <lesliejoe2k2@gmail.com>
2025-09-22 17:05:31 -0700

    added FAQ

--- src/components/FAQ.tsx
+++ src/components/FAQ.tsx
@@ -1,5 +1,57 @@
-const FAQ = () => {
-  return <div>FAQ</div>;
-};
+// const FAQ = () => {
+//   return <div>FAQ</div>;
+// };
 
-export default FAQ;
+// export default FAQ;
+
+import {
+  Disclosure,
+  DisclosureButton,
+  DisclosurePanel,
+} from "@headlessui/react";
+import { MinusIcon, PlusIcon } from "@heroicons/react/24/outline";
+import { faqs } from "../data";
+
+export default function FAQ() {
+  return (
+    <div className="bg-white">
+      <div className="mx-auto max-w-7xl px-6 py-12 sm:py-32 lg:px-8 lg:py-16">
+        <div className="mx-auto max-w-4xl">
+          <h2 className="text-4xl font-semibold tracking-tight text-gray-900 sm:text-5xl text-center">
+            Frequently asked questions
+          </h2>
+          <dl className="mt-16 divide-y divide-gray-900/10">
+            {faqs.map((faq) => (
+              <Disclosure
+                key={faq.question}
+                as="div"
+                className="py-6 first:pt-0 last:pb-0"
+              >
+                <dt>
+                  <DisclosureButton className="group flex w-full items-start justify-between text-left text-gray-900">
+                    <span className="text-lg font-semibold">
+                      {faq.question}
+                    </span>
+                    <span className="ml-6 flex h-7 items-center">
+                      <PlusIcon
+                        aria-hidden="true"
+                        className="size-6 group-data-open:hidden"
+                      />
+                      <MinusIcon
+                        aria-hidden="true"
+                        className="size-6 group-not-data-open:hidden"
+                      />
+                    </span>
+                  </DisclosureButton>
+                </dt>
+                <DisclosurePanel as="dd" className="mt-2 pr-12">
+                  <p className="text-lg text-gray-600">{faq.answer}</p>
+                </DisclosurePanel>
+              </Disclosure>
+            ))}
+          </dl>
+        </div>
+      </div>
+    </div>
+  );
+}
