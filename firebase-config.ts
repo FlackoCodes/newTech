58234b63eb79eba8fb31b9ba25cf641ac96fc4e8
Joseph Kofi Leslie Lartey <lesliejoe2k2@gmail.com>
2025-11-06 00:49:27 -0800

    saved keys in env variable

--- firebase-config.ts
+++ firebase-config.ts
@@ -1,17 +1,17 @@
 import { initializeApp } from "firebase/app";
-import { FIREBASE_API } from "./constants";
 import { getAuth } from "firebase/auth";
 
-
 const firebaseConfig = {
-  apiKey: FIREBASE_API,
-  authDomain: "new-tech-8eb3d.firebaseapp.com",
-  projectId: "new-tech-8eb3d",
-  storageBucket: "new-tech-8eb3d.firebasestorage.app",
-  messagingSenderId: "835612803851",
-  appId: "1:835612803851:web:5ec5e52849f55000fe580e",
-  measurementId: "G-5LN47YFLXZ"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
+
+export default firebaseConfig;
+
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
