dab2eeba73d590936f7e0f69f5bd487c8dd655a6
Joseph Kofi Leslie Lartey <lesliejoe2k2@gmail.com>
2025-11-14 11:13:38 +0000

    fixes on firebase config

--- firebase-config.ts
+++ firebase-config.ts
@@ -2,12 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  apiKey: "AIzaSyBGVhP6XUWcFPVdCCiXMsw9zZGnTQwRiTc",
+  authDomain: "new-tech-8eb3d.firebaseapp.com",
+  projectId: "new-tech-8eb3d",
+  storageBucket: "new-tech-8eb3d.firebasestorage.app",
+  messagingSenderId: "835612803851",
+  appId: "1:835612803851:web:5ec5e52849f55000fe580e",
+  measurementId: "1:835612803851:web:5ec5e52849f55000fe580e"
 };
 
 export default firebaseConfig;
